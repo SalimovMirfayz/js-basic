e9d2c71500de19922055f754b6cdb7a1b46799c9
SalimovMirfayz <148865250+SalimovMirfayz@users.noreply.github.com>
2023-11-07 10:53:56 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,69 @@
+
+//=======================================================//
+//Variables-o'zgaruvchilar
+
+
+//var-->eski-->2015gacha
+//let
+//const
+
+// var myName = "Mirfayz";
+// var myYear =  2007;
+// var myAge = 16;
+
+// myAge = 17;
+// myAge = 18;
+
+// myAge = myAge+1;
+
+// var newAge = myAge+myAge;
+
+// console.log(myName,myYear,myAge);
+// let myCity;
+
+// myCity="Bukhara";
+
+// console.log(myCity);
+
+// const myName="Mirfayz"
+// console.log(myName);
+//=========================================//
+//String comcatenation-stringni stirnga qo'shish
+
+
+// let text ="Hello";
+
+// console.log(text+ "world");
+// let myName ="Mirfayz"
+// let mySurename ="Salimov"
+// let myAge =16
+// myAge=myAge+1
+// let isUzbek=true;
+// console.log(`Hello, my name is ${myName} ${mySurename}. I am ${myAge} years old.`);
+
+//===================================================================//
+//Amallar: matematik amallar, taqqoslash amallar, mantiqiy amallar
+
+// +
+// -
+// *
+// /
+// %
+// **
+
+
+// let x=10;
+// x =x ** 2
+// console.log(`x: ${x}`);
+
+
+
+// let name = prompt("Ismingiz nima?")
+//  console.log(`Salom, ${name}. Mening ismim Javascript`);
+
+let age = prompt("Yoshingizni kiriting")
+age=2023-age
+console.log(`Siz ${age} yilda tug'ilgansiz`)
+let number = prompt("Oylik maoshingizni kiriting")
+number=0.12*number
+console.log(`Siz davlatga ${number} so'm miqdorida soliq to'lashingiz kerak `)
